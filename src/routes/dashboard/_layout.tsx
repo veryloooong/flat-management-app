476b0552f7d4413220b3f70e8a6575dc8c590bab
teckedboy <nguyenhoangviet.hsgs@gmail.com>
2024-09-30 14:28:00 +0700

    dhbk

--- src/routes/dashboard/_layout.tsx
+++ src/routes/dashboard/_layout.tsx
@@ -3,10 +3,16 @@ import { createFileRoute, Outlet } from '@tanstack/react-router'
 function DashboardLayoutPage(): JSX.Element {
   return (
     <div>
+      <div className="w-full h-16 bg-main-palette-5"></div>
       <h1>Dashboard Layout</h1>
       <p>This is the dashboard layout.</p>
       <Outlet />
+      <header>
+        <h1>ĐẠI HỌC BÁCH KHOA HÀ NỘI</h1>
+        <p>Hanoi University of Science and Technology</p>
+      </header>
     </div>
+    
   )
 }
 
